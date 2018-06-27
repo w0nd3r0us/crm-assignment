abb7abe77fc53f2d6afab4953388536b020b6af0
w0nd3r0us <steven.grewal@gmail.com>
2018-06-27 01:17:08 -0400

    integration of sqlite3 using active and mini record

--- contact.rb
+++ contact.rb
@@ -1,58 +1,21 @@
-class Contact
+gem 'activerecord', '=4.2.10'
+require "mini_record"
+require "active_record"
 
-  # This method should initialize the contact's attributes
-  def initialize
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'crm.sqlite3')
 
-  end
+class Contact < ActiveRecord::Base
 
-  # This method should call the initializer, 
-  # store the newly created contact, and then return it
-  def self.create
+  field :first_name, as: :string
+  field :last_name,  as: :string
+  field :email,      as: :string
+  field :note,       as: :text
 
-  end
-
-  # This method should return all of the existing contacts
-  def self.all
-
-  end
-
-  # This method should accept an id as an argument
-  # and return the contact who has that id
-  def self.find
-
-  end
-
-  # This method should allow you to specify 
-  # 1. which of the contact's attributes you want to update
-  # 2. the new value for that attribute
-  # and then make the appropriate change to the contact
-  def update
-
-  end
-
-  # This method should work similarly to the find method above
-  # but it should allow you to search for a contact using attributes other than id
-  # by specifying both the name of the attribute and the value
-  # eg. searching for 'first_name', 'Betty' should return the first contact named Betty
-  def self.find_by
-
-  end
-
-  # This method should delete all of the contacts
-  def self.delete_all
-
-  end
 
   def full_name
-
+    "#{first_name} #{last_name}"
   end
 
-  # This method should delete the contact
-  # HINT: Check the Array class docs for built-in methods that might be useful here
-  def delete
-
-  end
-
-  # Feel free to add other methods here, if you need them.
-  
 end
+
+Contact.auto_upgrade!
